f568a13e017521d03878755c00773083435affc1
Jesus Marroquin <jes_cvz@hotmail.com>
2024-11-14 00:20:55 -0600

    added navbar and sidebar for responsive design

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,11 +3,13 @@ import { routes } from '../../app.routes';
 import { MenuBarComponent } from "../menu-bar/menu-bar.component";
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SideBarMenuComponent } from "../side-bar-menu/side-bar-menu.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MenuBarComponent, MatButtonModule, MatIconModule],
+  imports: [MenuBarComponent, MatButtonModule, MatIconModule, MatSidenavModule, SideBarMenuComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
