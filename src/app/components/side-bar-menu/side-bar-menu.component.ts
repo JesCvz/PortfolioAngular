db6c4f6b6d6991fc8c29c8d056717bc191dee214
Jesus Marroquin <jes_cvz@hotmail.com>
2024-11-14 22:14:50 -0600

    Added routes to sidebar

--- src/app/components/side-bar-menu/side-bar-menu.component.ts
+++ src/app/components/side-bar-menu/side-bar-menu.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { MobileService } from '../../services/Mobile/mobile.service';
+import { routes } from '../../app.routes';
+import { MatListModule } from '@angular/material/list';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, RouterModule],
   templateUrl: './side-bar-menu.component.html',
   styleUrl: './side-bar-menu.component.css'
 })
@@ -14,18 +17,19 @@ export class SideBarMenuComponent {
   @Input() isSideBarOpen = false;
   @Output() isSideBarOpenChange = new EventEmitter<boolean>();
   isMobile = false;
+  routes = routes;
 
   constructor(
     private elementRef: ElementRef,
-    private isMobileService: MobileService
+    private isMobileService: MobileService,
+    private router: Router
   ){}
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
     const targetElement = event.target as HTMLElement;
     if (this.isSideBarOpen && !this.elementRef.nativeElement.contains(targetElement)) {
-      this.isSideBarOpen = false
-      this.isSideBarOpenChange.emit(this.isSideBarOpen);
+      this.handleSideBarClose()
     }
   }
 
@@ -35,4 +39,17 @@ export class SideBarMenuComponent {
     })
   }
 
+  handleRouteClick(value:string | undefined){
+    if(!value){
+      return;
+    } 
+    
+    this.router.navigate([value])
+    this.handleSideBarClose()
+  }
+
+  handleSideBarClose(){
+    this.isSideBarOpen = false
+    this.isSideBarOpenChange.emit(this.isSideBarOpen);
+  }
 }
